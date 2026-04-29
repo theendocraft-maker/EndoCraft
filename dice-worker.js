455736388014a943464e1d034598e1c9b1bb8692
Marco <theendocraft@gmail.com>
2026-04-30 01:30:40 +0200

    Deploy: 30.04.2026  1:30:40,08

--- dice-worker.js
+++ dice-worker.js
@@ -52,10 +52,10 @@ function init(cfg) {
 
   var dMat = new CANNON.Material('d');
   world.addContactMaterial(new CANNON.ContactMaterial(gMat, dMat, {
-    friction: 0.40, restitution: 0.65
+    friction: 0.58, restitution: 0.32
   }));
 
-  diceBody = new CANNON.Body({ mass: 1, material: dMat, linearDamping: 0.07, angularDamping: 0.20 });
+  diceBody = new CANNON.Body({ mass: 1, material: dMat, linearDamping: 0.12, angularDamping: 0.32 });
   diceBody.addShape(makeIcosahedron(cfg.r));
   diceBody.position.set(cfg.sx, cfg.sy || 2.5, cfg.sz);
   diceBody.velocity.set(cfg.vx, cfg.vy, cfg.vz);
